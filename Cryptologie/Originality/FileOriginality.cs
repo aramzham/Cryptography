a173ee17adf311099cfa822859221c7d665b1a01
aramzham <aramzham@hotmail.com>
2017-02-01 14:18:40 +0400

    file originality

---
+++ Cryptologie/Originality/FileOriginality.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Originality
+{
+    public class FileOriginality
+    {
+        private byte[] ComputeHMACSHA256(byte[] toBeHashed)
+        {
+            using (var hmac = new HMACSHA256())
+            {
+                try
+                {
+                    return hmac.ComputeHash(toBeHashed);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                    return new byte[] {};
+                }
+            }
+        }
+
+        public string GetHMACSHA256(string path)
+        {
+            var fileBytes = File.ReadAllBytes(path);
+            var hashBytes = ComputeHMACSHA256(fileBytes);
+            return Convert.ToBase64String(hashBytes);
+        }
+    }
+}
--- Cryptologie/Test/Program.cs
+++ Cryptologie/Test/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text;
 using Cryptography;
+using Originality;
 
 namespace Test
 {
@@ -8,60 +9,63 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            var originalMessage = "Original Message to hash";
-            var originalMessage2 = "This is another message to hash";
-            Console.WriteLine("HMAC Demonstration in .NET");
-            Console.WriteLine("--------------------------");
-            Console.WriteLine();
-            var key = HMAC.GenerateKey();
-            var hmacMd5Message =
-            HMAC.ComputeHMACMD5(Encoding.UTF8.GetBytes(originalMessage), key);
-            var hmacMd5Message2 =
-            HMAC.ComputeHMACMD5(Encoding.UTF8.GetBytes(originalMessage2), key);
-            var hmacSha1Message =
-            HMAC.ComputeHMACSHA1(Encoding.UTF8.GetBytes(originalMessage), key);
-            var hmacSha1Message2 =
-            HMAC.ComputeHMACSHA1(Encoding.UTF8.GetBytes(originalMessage2), key);
-            var hmacSha256Message =
-            HMAC.ComputeHMACSHA256(Encoding.UTF8.GetBytes(originalMessage), key);
-            var hmacSha256Message2 =
-            HMAC.ComputeHMACSHA256(Encoding.UTF8.GetBytes(originalMessage2), key);
-            var hmacSha512Message =
-            HMAC.ComputeHMACSHA512(Encoding.UTF8.GetBytes(originalMessage), key);
-            var hmacSha512Message2 =
-            HMAC.ComputeHMACSHA512(Encoding.UTF8.GetBytes(originalMessage2), key);
-            Console.WriteLine();
-            Console.WriteLine("MD5 HMAC");
-            Console.WriteLine();
-            Console.WriteLine("Message 1 hash = " +
-            Convert.ToBase64String(hmacMd5Message));
-            Console.WriteLine("Message 2 hash = " +
-            Convert.ToBase64String(hmacMd5Message2));
-            Console.WriteLine();
-            Console.WriteLine("SHA 1 HMAC");
-            Console.WriteLine();
-            Console.WriteLine("Message 1 hash = " +
-            Convert.ToBase64String(hmacSha1Message));
-            Console.WriteLine("Message 2 hash = " +
-            Convert.ToBase64String(hmacSha1Message2));
-            Console.WriteLine();
-            Console.WriteLine("SHA 256 HMAC");
-            Console.WriteLine();
-            Console.WriteLine("Message 1 hash = " +
-            Convert.ToBase64String(hmacSha256Message));
-            Console.WriteLine("Message 2 hash = " +
-            Convert.ToBase64String(hmacSha256Message2));
-            Console.WriteLine();
-            Console.WriteLine("SHA 512 HMAC");
-            Console.WriteLine();
-            Console.WriteLine("Message 1 hash = " +
-            Convert.ToBase64String(hmacSha512Message));
-            Console.WriteLine("Message 2 hash = " +
-            Convert.ToBase64String(hmacSha512Message2));
-            Console.WriteLine();
+            #region HMAC Demonstration
+            //var originalMessage = "Original Message to hash";
+            //var originalMessage2 = "This is another message to hash";
+            //Console.WriteLine("HMAC Demonstration in .NET");
+            //Console.WriteLine("--------------------------");
+            //Console.WriteLine();
+            //var key = HMAC.GenerateKey();
+            //var hmacMd5Message =
+            //HMAC.ComputeHMACMD5(Encoding.UTF8.GetBytes(originalMessage), key);
+            //var hmacMd5Message2 =
+            //HMAC.ComputeHMACMD5(Encoding.UTF8.GetBytes(originalMessage2), key);
+            //var hmacSha1Message =
+            //HMAC.ComputeHMACSHA1(Encoding.UTF8.GetBytes(originalMessage), key);
+            //var hmacSha1Message2 =
+            //HMAC.ComputeHMACSHA1(Encoding.UTF8.GetBytes(originalMessage2), key);
+            //var hmacSha256Message =
+            //HMAC.ComputeHMACSHA256(Encoding.UTF8.GetBytes(originalMessage), key);
+            //var hmacSha256Message2 =
+            //HMAC.ComputeHMACSHA256(Encoding.UTF8.GetBytes(originalMessage2), key);
+            //var hmacSha512Message =
+            //HMAC.ComputeHMACSHA512(Encoding.UTF8.GetBytes(originalMessage), key);
+            //var hmacSha512Message2 =
+            //HMAC.ComputeHMACSHA512(Encoding.UTF8.GetBytes(originalMessage2), key);
+            //Console.WriteLine();
+            //Console.WriteLine("MD5 HMAC");
+            //Console.WriteLine();
+            //Console.WriteLine("Message 1 hash = " +
+            //Convert.ToBase64String(hmacMd5Message));
+            //Console.WriteLine("Message 2 hash = " +
+            //Convert.ToBase64String(hmacMd5Message2));
+            //Console.WriteLine();
+            //Console.WriteLine("SHA 1 HMAC");
+            //Console.WriteLine();
+            //Console.WriteLine("Message 1 hash = " +
+            //Convert.ToBase64String(hmacSha1Message));
+            //Console.WriteLine("Message 2 hash = " +
+            //Convert.ToBase64String(hmacSha1Message2));
+            //Console.WriteLine();
+            //Console.WriteLine("SHA 256 HMAC");
+            //Console.WriteLine();
+            //Console.WriteLine("Message 1 hash = " +
+            //Convert.ToBase64String(hmacSha256Message));
+            //Console.WriteLine("Message 2 hash = " +
+            //Convert.ToBase64String(hmacSha256Message2));
+            //Console.WriteLine();
+            //Console.WriteLine("SHA 512 HMAC");
+            //Console.WriteLine();
+            //Console.WriteLine("Message 1 hash = " +
+            //Convert.ToBase64String(hmacSha512Message));
+            //Console.WriteLine("Message 2 hash = " +
+            //Convert.ToBase64String(hmacSha512Message2));
+            //Console.WriteLine();
 
-            Console.ReadLine();
+            //Console.ReadLine();
+            #endregion
 
+            #region DESEncryption Demonstration
             //var des = new DesEncryption();
             //var key = DesEncryption.GenerateRandomNumber(8);
             //var iv = DesEncryption.GenerateRandomNumber(8);
@@ -77,6 +81,12 @@ namespace Test
             //Convert.ToBase64String(encrypted));
             //Console.WriteLine("Decrypted Text = " + decryptedMessage);
             //Console.ReadLine();
+            #endregion
+
+            var fo = new FileOriginality();
+            Console.WriteLine(fo.GetHMACSHA256(@"C:\Users\HP\Desktop\emails.txt"));
+
+            Console.ReadKey();
         }
     }
 }
